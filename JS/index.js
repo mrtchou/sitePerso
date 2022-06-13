6e3ae453406aecf936b31a5fb2ed2dbbc33dae7d
TCH <saidkhajtchoutchaiev@MacBook-Air-de-SAIDKHAJ.local>
2022-06-13 23:30:01 +0200

    modification navbar page bus.html amelioration menu mobile, ajout gasbn de js pour chargement un par un animation

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,29 @@ var cards = document.querySelectorAll('.card');
         card.classList.toggle('is-flipped');
     });
 });
+
+
+const scrollIndicator = document.querySelector(".scroll-indicator");
+const content = document.querySelector(".blocartes");
+
+const observer = new IntersectionObserver(handleIntersect);
+
+observer.observe(content);
+
+function handleIntersect(entries) {
+    const el = entries[0];
+    if (el.isIntersecting) {
+        window.addEventListener(".scroll", indicatorAnimation)
+    } else if (!el.isIntersecting) {
+        window.removeEventListener("scroll", indicatorAnimation)
+    }
+}
+
+function indicatorAnimation() {
+    if (window.scrollY > content.offsetTop) {
+        const percentage = ((window.scrollY - content.offsetTop) / content.scrollHeight * 100);
+        scrollIndicator.getElementsByClassName.transforme = `scaleX(${(percentage / 100)})`
+    } else {
+        scrollIndicator.getElementsByClassName.transforme = "scaleX(0)";
+    }
+}
