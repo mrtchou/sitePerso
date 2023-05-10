c3a675bc138cce098e2508d8727743a6aa9b9afb
TCH <saidkhajtchoutchaiev@MacBook-Air-de-SAIDKHAJ.local>
2023-05-10 12:10:15 +0200

    amelioration menu navbar css qui depassait l'ecran, j'ai ajouté la ligne box sizing pour tout contenir

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,33 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <header>
+      <nav className="navbar">
+        <div className="logo">
+          <h1>BEPAG</h1>
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portofolio</a>
+          </li>
+          <li>
+            <a href="#projects">Nos Projets</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#login">Connexion</a>
+          </li>
+          <li>
+            <a href="#register">Inscription</a>
+          </li>
+        </ul>
+      </nav>
       <div className="header-content">
-        <h1>BEPAG</h1>
-        <p>Best in Web Development and Progressive Solutions</p>
+        <h2>Best in Web Development and Progressive Solutions</h2>
       </div>
     </header>
   );
