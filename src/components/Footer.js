c8e7a0c17debd09fe03c190a31c94a7da3bd5b9b
mrTchou <dev.mrtchou@gmail.com>
2023-05-11 23:00:45 +0200

    partie footer css, reprise de grid vers flex

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,60 +2,74 @@ import React from "react";
 import "../styles/Footer.css";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer>
-      <div className="footer-content">
+      <div className="footer-left">
+        <h4>Contact</h4>
+        <ul>
+          <li>
+            Email: <br />
+            <a href="mailto:contact@bepag.fr">contact@bepag.fr</a>
+          </li>
+          <br />
+          <br />
+          <li>
+            Adresse: <br />
+            24 rue Edmond Bonte
+            <br />
+            91130 Ris Orangis
+            <br /> France
+          </li>
+        </ul>
+      </div>
+      <div className="footer-center">
         <h3>BEPAG</h3>
         <p>
           Business Evolution - Progressive Advancement Group
           <br />
+          <br />
           Développement Web de premier plan
         </p>
       </div>
-      <div className="footer-content">
-        <h4>Contact</h4>
-        <ul>
-          <li>
-            Email: <a href="mailto:contact@bepag.fr">contact@bepag.fr</a>
-          </li>
-          <li>Téléphone: +33 (0)1 23 45 67 89</li>
-          <li>Adresse: 123 rue des Exemples, 75000 Paris, France</li>
-        </ul>
+      <div className="footer-right">
+        <div>
+          <h4>Liens</h4>
+          <ul>
+            <li>
+              <a href="/">Accueil</a>
+            </li>
+            <li>
+              <a href="/portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="/projets">Nos Projets</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>Réseaux sociaux</h4>
+          <ul className="socials">
+            <li>
+              <a href="https://www.facebook.com">Facebook</a>
+            </li>
+            <li>
+              <a href="https://twitter.com">Twitter</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com">Instagram</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="footer-content">
-        <h4>Liens</h4>
-        <ul>
-          <li>
-            <a href="/">Accueil</a>
-          </li>
-          <li>
-            <a href="/portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="/projets">Nos Projets</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-      <div className="footer-content">
-        <h4>Réseaux sociaux</h4>
-        <ul className="socials">
-          <li>
-            <a href="https://www.facebook.com">Facebook</a>
-          </li>
-          <li>
-            <a href="https://twitter.com">Twitter</a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com">LinkedIn</a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com">Instagram</a>
-          </li>
-        </ul>
-      </div>
+      <div className="footer-bottom">Copyright BEPAG {year}</div>
     </footer>
   );
 };
