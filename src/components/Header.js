bddf82226f76911483dddc824345139e0976ff0c
mrTchou <dev.mrtchou@gmail.com>
2023-05-17 21:22:21 +0200

    add comments in code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,32 @@
-// On importe les bibliothèques nécessaires : React, le module Link de react-router-dom pour la navigation, et le module useLocation pour accéder à l'emplacement actuel dans l'application.
+// Importation des bibliothèques et modules nécessaires
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet";
-
-// On importe le CSS pour le style du Header.
 import "../styles/Header.css";
 
-// On définit le composant Header qui prend un objet 'props' en argument, ici nous utilisons la destructuration pour extraire 'small' directement.
 const Header = ({ small }) => {
-  // On utilise le hook useLocation pour obtenir un objet représentant l'emplacement actuel (URL).
+  // Obtenez l'emplacement actuel (URL) grâce au hook useLocation
   const location = useLocation();
+  // Initialisation d'un état pour gérer la visibilité du contenu de l'en-tête
   const [isVisible, setIsVisible] = useState(false);
 
-  // On définit le titre à afficher dans le Header.
+  // Les titres sont définis ici
   const title = "Best in Web Development and Progressive Solutions";
   const titleBepag = "BEPAG";
 
-  // On sépare le titre en un tableau de lettres pour permettre l'animation de chaque lettre individuellement.
+  // Les titres sont divisés en un tableau de lettres pour permettre l'animation de chaque lettre individuellement.
   const letters = title.split("");
   const lettersBepag = titleBepag.split("");
 
   useEffect(() => {
+    // Utilisation d'un effet pour définir la visibilité du contenu de l'en-tête après un certain délai
     setTimeout(() => {
       setIsVisible(true);
-    }, 130); // Attend 1 seconde avant de rendre le paragraphe visible
+    }, 130); // Attend 130ms avant de rendre le paragraphe visible
   }, []);
 
-  // On renvoie le JSX pour le Header.
+  // Le code JSX pour le Header est retourné ici
   return (
-    // On applique une classe différente au Header en fonction de la route actuelle, pour changer le style du Header sur la page Contact.
     <header
       className={
         location.pathname === "/contact" ||
@@ -38,18 +36,21 @@ const Header = ({ small }) => {
           : ""
       }
     >
+      {/* Balises meta pour le SEO */}
       <Helmet>
         <meta
           name="description"
-          content="BEPAG est une agence spécialisée en développement web. Nous créons des sites web et applications SaaS performants pour tous secteurs, optimisant vos opérations et favorisant votre croissance. Nous maîtrisons React, Node.js, PHP, Symfony et WordPress et réalisons une grande diversité de projets, du site vitrine à l'application web complexe. Chez BEPAG, nous transformons vos idées en réalité avec des solutions adaptées à vos besoins spécifiques."
+          content="BEPAG est une agence spécialisée en développement web. Nous créons des sites web, des sites e-commerce et applications SaaS, optimisant vos opérations et favorisant votre croissance. Nous maîtrisons React, Node.js, PHP, Symfony et WordPress et réalisons une grande diversité de projets, du site vitrine à l'application web complexe. Chez BEPAG, nous transformons vos idées en réalité avec des solutions adaptées à vos besoins spécifiques."
         />
         <meta
           name="keywords"
           content="développement web, sites web, applications SaaS, React, Node.js, PHP, Symfony, WordPress"
         />
       </Helmet>
+      {/* Barre de navigation */}
       <nav className={`navbar`}>
         <div className="logo">
+          {/* Le logo est généré lettre par lettre */}
           <h1>
             {lettersBepag.map((lettr, index) => (
               <span key={index} className="hoverable-letter">
@@ -58,6 +59,7 @@ const Header = ({ small }) => {
             ))}
           </h1>
         </div>
+        {/* Menu */}
         <ul className="menu">
           {/* On utilise le composant Link pour la navigation dans l'application. */}
           <li>
@@ -72,31 +74,15 @@ const Header = ({ small }) => {
           <li>
             <Link to="/Nos-Services">Nos Services</Link>
           </li>
-          {/* <li>
-            <Link to="/projects">Nos Projets</Link>
-          </li>
-          <li>
-            <Link to="/login">Connexion</Link>
-          </li>
-          <li>
-            <Link to="/register">Inscription</Link>
-          </li> */}
         </ul>
       </nav>
 
+      {/* Contenu de l'en-tête */}
       <div className={`header-content ${isVisible ? "visible" : ""}`}>
-        {/* <h2>
-          {/* On parcourt le tableau de lettres et on affiche chaque lettre dans une balise span pour pouvoir animer chaque lettre individuellement. }
-          {letters.map((letter, index) => (
-            <span key={index} className="hoverable-letter">
-              {letter}
-            </span>
-          ))}
-        </h2> */}
+        {/* Texte principal */}
         <p>
           <strong>BEPAG</strong>, votre partenaire pour des solutions web sur
           mesure.
-          <br />
           <br />
           Nous transformons vos idées en réalité avec des solutions adaptées à
           vos besoins spécifiques.
@@ -106,5 +92,4 @@ const Header = ({ small }) => {
   );
 };
 
-// On exporte le composant Header pour pouvoir l'utiliser ailleurs dans l'application.
 export default Header;
