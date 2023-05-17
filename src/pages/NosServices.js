1b3812c388a361935abbc04f9e7ae987c301ed7c
mrTchou <dev.mrtchou@gmail.com>
2023-05-17 13:21:27 +0200

    add text services

--- src/pages/NosServices.js
+++ src/pages/NosServices.js
@@ -15,6 +15,31 @@ const Services = () => {
       description:
         "Que vous ayez besoin d'un site web corporatif, d'une boutique en ligne ou d'un portfolio personnel, nous avons les compétences et l'expertise nécessaires pour créer un site web qui correspond parfaitement à vos besoins. Nous utilisons une combinaison de HTML, CSS, JavaScript, React, et Node.js pour créer des sites web attrayants et fonctionnels.",
     },
+    {
+      title: "2. Développement d'applications web",
+      description:
+        "Si vous cherchez à créer une application web sur mesure, vous êtes au bon endroit. Nous utilisons les dernières technologies, y compris React et Node.js, pour créer des applications web progressives qui sont à la fois puissantes et intuitives.",
+    },
+    {
+      title: "3. E-commerce",
+      description:
+        "Vendez vos produits en ligne avec une boutique e-commerce sur mesure. Nous créerons une boutique en ligne optimisée pour les conversions, facile à gérer, et sécurisée.",
+    },
+    {
+      title: "4. Référencement (SEO)",
+      description:
+        "Un beau site web ne sert à rien s'il n'est pas visible. C'est pourquoi nous intégrons le SEO dès le début du processus de conception de votre site web. Nous utilisons les meilleures pratiques de SEO pour aider votre site web à atteindre les premiers rangs sur Google.",
+    },
+    {
+      title: "5. Maintenance et Support",
+      description:
+        "Votre site web a besoin d'un entretien régulier pour rester en bonne santé. Nous offrons des services de maintenance et de support pour assurer la longévité de votre site web. De plus, nous sommes toujours là pour vous aider avec des problèmes techniques ou des mises à jour du site.",
+    },
+    {
+      title: "6. Formation et Accompagnement",
+      description:
+        "Nous ne nous contentons pas de créer votre site web et de vous laisser à vous-même. Nous offrons également des formations sur l'utilisation et la gestion de votre nouveau site web. De plus, nous sommes là pour vous accompagner et vous conseiller sur votre présence en ligne.",
+    },
     // Ajoutez ici plus de services comme l'exemple ci-dessus
   ];
 
