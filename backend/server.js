0a5650397b6e95f16c29ef5e4b09f56eff81338c
mrTchou <dev.mrtchou@gmail.com>
2023-05-24 20:50:38 +0200

    ne route POST pour votre formulaire de contact

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.post("/contact", (req, res) => {
+  res.send(req.body);
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
