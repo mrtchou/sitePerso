1c3d735d998a169c510028fd3dda1a983604e7b1
mrTchou <dev.mrtchou@gmail.com>
2023-05-24 22:54:52 +0200

    changement server pour accepter tous les cors. add cors express nodemailer

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const nodemailer = require("nodemailer");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Configuration explicite de CORS
+app.use(
+  cors({
+    origin: "*", // autoriser toutes les origines
+    methods: "*", // autoriser toutes les méthodes
+    allowedHeaders: "*", // autoriser tous les headers
+  })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post("/contact", (req, res) => {
   res.send(req.body);
+  res.status(200).send(req.body);
 });
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
