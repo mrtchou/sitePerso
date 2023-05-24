804cb6d6ea1700f448279c83f619eca9934d0777
mrTchou <dev.mrtchou@gmail.com>
2023-05-24 17:27:46 +0200

    modification server.js pour traiter cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
