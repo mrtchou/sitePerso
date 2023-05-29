7951436ea3fd4f9d6afcb977337eddbd0df44ee5
mrTchou <dev.mrtchou@gmail.com>
2023-05-29 13:13:26 +0200

    modification fichier App.js pour inclure une logique avec mot de passe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 
 import Contact from "./components/Contact";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import About from "./pages/About";
-import Services from "./pages/NosServices";
+import Services from "./pages/Services";
 import ChildTasks from "./pages/ChildTasks";
+import PasswordPage from "./pages/PasswordPage";
 
 function App() {
+  const [password, setPassword] = useState("");
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -20,14 +23,21 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
-          <Route path="/Nos-Services" element={<Services />} />
-          <Route path="/child-tasks" element={<ChildTasks />} />
-
-          {/* <Route path="/signup" element={<Signup />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/articles" element={<Articles />} /> */}
+          <Route path="/services" element={<Services />} />
+          <Route
+            path="/password"
+            element={<PasswordPage setPassword={setPassword} />}
+          />
+          <Route
+            path="/child-tasks"
+            element={
+              password === "12345" ? (
+                <ChildTasks />
+              ) : (
+                <Navigate to="/password" />
+              )
+            }
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
